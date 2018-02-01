2979ed395c8f87ab53f615b58dfb5ca1e12a7cef
kshiprashetty <kshiprashetty@gmail.com>
2018-02-01 17:44:31 +0530

    added scripts to scripts directory

--- database.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-psql postgres  < init-database.sql
--- init-database.sql
+++
@@ -1,4 +0,0 @@
-CREATE ROLE database_admin WITH LOGIN PASSWORD 'password';
-ALTER ROLE database_admin CREATEDB;
-CREATE DATABASE socialscore_dev;
-GRANT ALL PRIVILEGES ON DATABASE socialscore_dev TO database_admin;
