af5c2c6c5d710d7ddd3f992b24268e0de5e5e65e
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2018-02-01 12:49:59 +0000

    Code Review: Issue 15: Initial Setup: Decide on backend framework (#34)
    * Issue 15: Initial Setup: Decide on backend framework
    
    * Additional routes and testing
    
    * renamed dashboard to index.html
    
    * new line at end of package.json

---
+++ src/server.js
@@ -0,0 +1,58 @@
+/**
+ * The index.js file aggregates the routing logic and starts the server
+ */
+const hapi = require('hapi');
+const inert = require('inert');
+
+// Create a new Hapi server
+const server = new hapi.Server();
+server.connection({
+  port: Number(process.env.PORT || 8080),
+  host: 'localhost',
+});
+
+/**
+ * Host the public folder statically
+ */
+server.register(inert, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{file*}',
+    handler: {
+      directory: {
+        path: 'public/',
+      },
+    },
+  });
+});
+
+server.route({
+  method: 'GET',
+  path: '/admin',
+  handler: (request, response) => {
+    const dummyJson = [
+      { first_name: 'fn1', last_name: 'ln1' },
+      { first_name: 'fn2', last_name: 'ln2' },
+    ];
+    response(JSON.stringify(dummyJson))
+      .header('Content-Type', 'application/json');
+  },
+});
+
+/**
+ * Start the server
+ */
+if (!module.parent) {
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+  });
+}
+
+module.exports = server;
---
+++ test/server.test.js
@@ -0,0 +1,33 @@
+const server = require('../src/server');
+const supertest = require('supertest');
+
+describe('Server should have the route', () => {
+  it('/login.html', (done) => {
+    supertest(server.listener)
+      .get('/login.html')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      })
+      .catch(console.log);
+  });
+  it('/index.html', (done) => {
+    supertest(server.listener)
+      .get('/index.html')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      })
+      .catch(console.log);
+  });
+  it('/admin', (done) => {
+    supertest(server.listener)
+      .get('/admin')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      })
+      .catch(console.log);
+  });
+});
+
