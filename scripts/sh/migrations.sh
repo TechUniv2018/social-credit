18925ae9230d0e86c129d5caf2a0a4593bdb9dda
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-02-05 18:51:01 +0530

    changed migrations.sh

--- scripts/sh/migrations.sh
+++ scripts/sh/migrations.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 node_modules/.bin/sequelize init && \
-cat scripts/json/config.json > config/config.json && \
 node_modules/.bin/sequelize model:generate --name User --attributes firstName:string,lastName:string,email:string && \
 node_modules/.bin/sequelize db:migrate
