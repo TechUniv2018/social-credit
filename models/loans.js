2df4bc67887794ff3c8993f21fab230d73a9e142
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-02-05 19:19:25 +0530

    added migrations for loan table

---
+++ models/loans.js
@@ -0,0 +1,17 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const loans = sequelize.define('loans', {
+    userID: DataTypes.INTEGER,
+    loanID: DataTypes.INTEGER,
+    outstandingAmount: DataTypes.FLOAT,
+    totalAmount: DataTypes.FLOAT,
+    installmentCount: DataTypes.INTEGER,
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+      },
+    },
+  });
+  return loans;
+};
