4fce3ac8733ecfbd91bb87106b4e8a51f291ee52
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-07 15:42:28 +0530

    Login UI added

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import './css/App.css';
 import Login from './Login';
 import Dashboard from './Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Social Credit</h1>
-      </header>
-      <div className="App-intro">
-        <Login />
-        <Dashboard />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isLoggedIn = false;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Social Credit</h1>
+        </header>
+        <div className="App-content">
+          <Login isLoggedIn={this.isLoggedIn} />
+          <Dashboard isLoggedIn={this.isLoggedIn} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -7,7 +7,11 @@ import Login from '../Login';
 configure({ adapter: new Adapter() });
 
 describe('Login component should have', () => {
-  const wrapper = shallow(<Login />);
+  const boolWrapper = true;
+  const wrapper = shallow(<Login isLoggedIn={boolWrapper} />);
+  it('a wrapper for all login buttons', () => {
+    expect(wrapper.find('.login-wrapper').length).toBe(1);
+  });
   it('a wrapper for FB login button', () => {
     expect(wrapper.find('.login-wrapper-fb').length).toBe(1);
   });
