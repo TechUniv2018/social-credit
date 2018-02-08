4db97b28a951174bc62fd9f6aabd0f885aaad1fe
kshiprashetty <kshiprashetty@gmail.com>
2018-02-08 20:26:25 +0530

    Issue 14: Post added

--- src/routes/api/max-amount.js
+++ src/routes/api/max-amount.js
@@ -16,4 +16,39 @@ module.exports = [{
     });
   },
 },
+// {
+//   path: '/api/maxAmount',
+//   method: 'PATCH',
+//   handler: (request, response) => {
+//
+//     request({
+//       data:
+//     })
+//     model.banks.findOne().then((bank) => {
+//       if (bank === null) {
+//    throw new Error(`Could not find bank`);
+//     })
+//   },
+// },
+{
+  method: 'POST',
+  path: '/api/maxAmount',
+  handler: (request, reply) => {
+    const amountFromAdmin = request.payload.amount;
+    const currencyFromAdmin = request.payload.currency;
+    // model.banks.destroy({
+    //   where: {},
+    //   truncate: true,
+    // });
+    model.banks.findOne().then((bankDetails) => {
+      bankDetails.updateAttributes({
+        amount: amountFromAdmin,
+        currency: currencyFromAdmin,
+      });
+      const response = reply(`${bankDetails.amount}`);
+      // const response = reply(`Amount set: ${amount}\nCurrency set: ${currency}`);
+      response.header('Content-Type', 'text/plain');
+    });
+  },
+},
 ];
--- test/server.test.js
+++ test/server.test.js
@@ -29,23 +29,4 @@ describe('Server should have the route', () => {
       })
       .catch(console.log);
   });
-  it('/api/maxAmount', (done) => {
-    supertest(server.listener)
-      .get('/api/maxAmount')
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-  describe('With contents in', () => {
-    it('/api/maxAmount', (done) => {
-      supertest(server.listener)
-        .get('/api/maxAmount')
-        .then((response) => {
-          const resultKeys = ['amount', 'currency', 'createdAt', 'updatedAt', 'id'];
-          expect(Object.keys(response.body).sort()).toEqual(resultKeys.sort());
-          done();
-        });
-    });
-  });
 });
