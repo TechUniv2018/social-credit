f2f6f44a0a5433c8bd0a85b3ee0344779c994a07
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-02-09 18:30:38 +0530

    removed eslint errors

--- models/loans.js
+++ models/loans.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     installmentCount: DataTypes.INTEGER,
   }, {
     classMethods: {
-      associate(models) {
+      associate() {
         // associations can be defined here
       },
     },
