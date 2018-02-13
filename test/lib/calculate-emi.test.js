aa2eea3c1b212dea705085ef4039ad6b74b5d35c
kshiprashetty <kshiprashetty@gmail.com>
2018-02-13 20:33:31 +0530

    ISSUE 13: Added test case for checking if promise is resolved

--- test/lib/calculate-emi.test.js
+++ test/lib/calculate-emi.test.js
@@ -1,5 +1,5 @@
 const { calculateEmi } = require('../../src/lib/calculate-emi');
-// const models = require('../../models');
+const models = require('../../models');
 
 describe('calculateEmi', () => {
   test('should return a Promise', () => {
@@ -28,6 +28,17 @@ describe('calculateEmi', () => {
         .then(() => {
         }).catch((e) => {
           expect(e.message).toEqual('Invalid Id');
+          done();
+        });
+    });
+  });
+  describe('Given valid id', () => {
+    test('should resolve with emi', (done) => {
+      expect.assertions(1);
+
+      calculateEmi(1, 1)
+        .then((emi) => {
+          expect(typeof (emi)).toBe('number');
           done();
         });
     });
