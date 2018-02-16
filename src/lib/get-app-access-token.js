14e2dd40d9fc20165c77f5b9808ce320526288eb
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-16 18:49:06 +0530

    get-app-access-token implemented

--- src/lib/get-app-access-token.js
+++ src/lib/get-app-access-token.js
@@ -1,6 +1,17 @@
+const rp = require('request-promise');
+
+/**
+ * This function reads the CLIENT_ID and CLIENT_SECRET environment variables
+ * to use environment variables, type
+ * `CLIENT_ID={your-client-id} CLIENT_SECRET={your-client-secret} npm start`
+ * in shell
+ */
 const getAppAccessToken = () => {
-  const accessToken = '';
-  return Promise.resolve(accessToken);
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const requestUrl = `https://graph.facebook.com/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
+  return rp(requestUrl)
+    .then(response => JSON.parse(response).access_token);
 };
 
 module.exports = getAppAccessToken;
