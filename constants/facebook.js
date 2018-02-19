4b3fa1c6f5c05adc087ea9645988704a9d8b5b09
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-02-19 18:32:19 +0530

    Added helper function to retrieve Facebook user data

--- constants/facebook.js
+++ constants/facebook.js
@@ -1,5 +1,5 @@
 module.exports = {
   getAccessToken: 'https://graph.facebook.com/oauth/access_token?grant_type=client_credentials',
   debugToken: 'https://graph.facebook.com/debug_token',
+  userData: 'https://graph.facebook.com/me?fields=id,first_name,last_name',
 };
-
