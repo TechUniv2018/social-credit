58c98e1373efd8708f12f8f03fc79ee9c7877a53
Sahu, S <sauravsahu.12@live.com>
2018-03-03 01:06:12 +0530

    add seeder to add facebookUserId to facebooks table, for testing
    add seeder to add facebookUserId to facebooks table, for testing

---
+++ seeders/20180302165750-seed-facebook-users.js
@@ -0,0 +1,19 @@
+
+
+module.exports = {
+  up: queryInterface => queryInterface.bulkInsert('facebooks', [
+    {
+      id: '1805116396205254',
+      userId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '1623984961019175',
+      userId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]),
+  down: queryInterface => queryInterface.bulkDelete('facebooks'),
+};
