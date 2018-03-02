acd947b863cc386d574de0686f51c837c902cc47
Sahu, S <sauravsahu.12@live.com>
2018-03-03 01:11:08 +0530

    Fix tests: replace userId with access token. For tests, pass accesstoken using environment variable.
    Fix tests: replace userId with access token. For tests, pass accesstoken using environment variable.

--- src/routes/api/users/loans.js
+++ src/routes/api/users/loans.js
@@ -4,8 +4,7 @@ const models = require('../../../../models');
 const userLoansHelper = require('../../../../src/lib/user-helpers').userLoans;
 const facebookHelpers = require('../../../lib/facebook-helpers');
 
-module.exports = [];
-const loans = [
+module.exports = [
   {
     path: '/api/users/loans',
     method: 'GET',
@@ -21,87 +20,36 @@ const loans = [
         },
       },
     },
-    handler: (request, response) => {
-      facebookHelpers.inspectUserAccessToken(request.headers.accesstoken)
-        .then((inspectionResult) => {
-          if (inspectionResult.isValid) {
-            models.facebooks.find({
-              where: {
-                userId: inspectionResult.userId,
-              },
-            })
-              .then(facebookUser => models.users.find({
-                where: {
-                  id: facebookUser.userId,
-                },
-              }))
-              .then((user) => {
-                userLoansHelper(user.id)
-                  .then((loans) => {
-                    const loansMapped = loans.map(loan => ({
-                      id: loan.id,
-                      totalAmount: loan.totalAmount,
-                      outstandingAmount: loan.outstandingAmount,
-                      createdAt: loan.createdAt,
-                    }));
-
-                    response({
-                      data: loansMapped,
-                      statusCode: 200,
-                    });
-                  })
-                  .catch(() => {
-                    response({
-                      data: {
-                        reason: 'Invalid user access token',
-                      },
-                      statusCode: 400,
-                    });
-                  });
-              });
-          }
-        })
-        .catch(() => {
-          response({
-            error: 'Unable to retrieve user loans.',
-            statusCode: 500,
-          });
-        });
-    },
-  },
-  {
-    path: '/api/users/loans',
-    method: 'POST',
-    config: {
-      description: 'DEBUG: Add a new loan entry for a particular user.',
-      tags: ['api'],
-      validate: {
-        payload: {
-          data: joi.object({
-            userId: joi.number().required(),
-            outstandingAmount: joi.number().required(),
-            outstandingInstallments: joi.number().required(),
-            totalAmount: joi.number().required(),
-            totalInstallments: joi.number().required(),
-          }).required(),
-        },
-      },
-    },
-    handler: (request, response) => {
-      models.loans.create(request.payload.data)
-        .then(() => {
-          response({
-            statusCode: 201,
-          });
-        })
-        .catch((e) => {
-          response({
-            data: {
-              reason: e.message,
+    handler: (request, response) => facebookHelpers
+      .inspectUserAccessToken(request.headers.accesstoken)
+      .then((inspectionResult) => {
+        if (inspectionResult.isValid) {
+          return models.facebooks.find({
+            where: {
+              id: inspectionResult.userId,
             },
-            statusCode: e.message === 'Invalid userId.' ? 400 : 500,
-          });
+          })
+            .then(facebookUser => userLoansHelper(facebookUser.userId))
+            .then((loans) => {
+              const loansMapped = loans.map(loan => ({
+                id: loan.id,
+                totalAmount: loan.totalAmount,
+                outstandingAmount: loan.outstandingAmount,
+                createdAt: loan.createdAt,
+              }));
+
+              response({
+                data: loansMapped,
+                statusCode: 200,
+              });
+            });
+        }
+        return response({
+          error: 'Invalid token',
+          message: 'Invalid user access token',
+          statusCode: 400,
         });
-    },
+      })
+      .catch(response),
   },
 ];
