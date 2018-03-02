753f5ecb7b1f2e1135f6c4263c9315a192eb289a
Sahu, S <sauravsahu.12@live.com>
2018-03-03 01:11:08 +0530

    Fix tests: replace userId with access token. For tests, pass accesstoken using environment variable.
    Fix tests: replace userId with access token. For tests, pass accesstoken using environment variable.

--- test/routes/api/users/loans.test.js
+++ test/routes/api/users/loans.test.js
@@ -2,11 +2,12 @@ const supertest = require('supertest');
 const models = require('../../../../models');
 const server = require('../../../../src/server');
 
-describe('route /api/users/{userId}/loans', () => {
+describe('route GET /api/users/loans', () => {
   describe('should return 200 statusCode', () => {
-    test('when valid userId is passed', (done) => {
+    test('when access token is valid', (done) => {
       supertest(server.listener)
-        .get('/api/users/1/loans')
+        .get('/api/users/loans')
+        .set('accesstoken', process.env.ACCESS_TOKEN)
         .then((response) => {
           expect(response.body.statusCode).toBe(200);
           done();
@@ -16,15 +17,15 @@ describe('route /api/users/{userId}/loans', () => {
   });
 
   describe('should return 400 statusCode', () => {
-    test('when invalid userId is passed', (done) => {
+    test('when access token is invalid', (done) => {
       supertest(server.listener)
-        .get('/api/users/-120/loans')
+        .get('/api/users/loans')
+        .set('accesstoken', 'some-invalid-token')
         .then((response) => {
           expect(response.body).toEqual({
+            error: 'Invalid token',
+            message: 'Invalid user access token',
             statusCode: 400,
-            data: {
-              reason: 'Invalid userId.',
-            },
           });
           done();
         })
@@ -33,9 +34,10 @@ describe('route /api/users/{userId}/loans', () => {
   });
 
   describe('should return 200 statusCode', () => {
-    test('when valid userId is passed', (done) => {
+    test('when access token is valid', (done) => {
       supertest(server.listener)
-        .get('/api/users/1/loans')
+        .get('/api/users/loans')
+        .set('accesstoken', process.env.ACCESS_TOKEN)
         .then((response) => {
           expect(response.body.statusCode).toBe(200);
           done();
@@ -43,23 +45,24 @@ describe('route /api/users/{userId}/loans', () => {
         .catch((e) => { throw e; });
     });
   });
-});
 
-describe('should return correct number of loan objects', () => {
-  test('when valid userId is passed', (done) => {
-    supertest(server.listener)
-      .get('/api/users/1/loans')
-      .then(response =>
-        models.loans.count({
-          where: {
-            userId: 1,
-          },
-        })
-          .then((count) => {
-            expect(response.body.data.length).toBe(count);
-            done();
+  describe('should return correct number of loan objects', () => {
+    test('when access token is valid', (done) => {
+      supertest(server.listener)
+        .get('/api/users/loans')
+        .set('accesstoken', process.env.ACCESS_TOKEN)
+        .then(response =>
+          models.loans.count({
+            where: {
+              userId: 1,
+            },
           })
-          .catch((e) => { throw e; }))
-      .catch((e) => { throw e; });
+            .then((count) => {
+              expect(response.body.data.length).toBe(count);
+              done();
+            })
+            .catch((e) => { throw e; }))
+        .catch((e) => { throw e; });
+    });
   });
 });
