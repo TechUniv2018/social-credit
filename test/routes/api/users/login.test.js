109fc7fb92ee7c957b31ebd7935772620210443d
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-04 19:34:08 +0530

    Added login test for success

--- test/routes/api/users/login.test.js
+++ test/routes/api/users/login.test.js
@@ -1,5 +1,6 @@
 const supertest = require('supertest');
 const server = require('../../../../src/server');
+const models = require('../../../../models');
 
 describe('route /api/users/login', () => {
   describe('should return 200 statusCode', () => {
@@ -12,6 +13,29 @@ describe('route /api/users/login', () => {
           done();
         })
         .catch((e) => { throw e; });
+    });
+
+    it('Should create a new user when a valid token is passed', (done) => {
+      models.users.destroy({
+        where: {
+          firstName: 'Sachi',
+        },
+      })
+        .then(() => models.facebooks.destroy({
+          where: {
+            id: '2051629538452614',
+          },
+        }))
+        .then(() => {
+          supertest(server.listener)
+            .post('/api/users/login')
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(200);
+              expect(response.body.success).toBeTruthy();
+              done();
+            });
+        });
     });
   });
 });
