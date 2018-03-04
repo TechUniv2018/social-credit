8f4ae76f8637eb837c58f81d92262e9e4ba51be8
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-04 18:13:20 +0530

    Added numberOfFbFriends to the /api/users/info route

--- test/routes/api/users/info.test.js
+++ test/routes/api/users/info.test.js
@@ -1,4 +1,8 @@
 const supertest = require('supertest');
+const { matchers } = require('jest-json-schema');
+
+expect.extend(matchers);
+
 const server = require('../../../../src/server');
 
 describe('/api/users/info', () => {
@@ -8,6 +12,37 @@ describe('/api/users/info', () => {
       .set('accesstoken', 'INVALID')
       .then((response) => {
         expect(response.body.statusCode).toBe(401);
+        done();
+      })
+      .catch((e) => { throw e; });
+  });
+
+  it('when a valid accesstoke is passed, it should match schema', (done) => {
+    const schema = {
+      properties: {
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+        socialScore: { type: 'number' },
+        socialScoreBreakdown: {
+          properties: {
+            facebook: {
+              properties: {
+                numberOfFbFriends: { type: 'number' },
+              },
+              required: ['numberOfFbFriends'],
+            },
+          },
+          required: ['facebook'],
+        },
+      },
+      required: ['firstName', 'lastName', 'socialScore', 'socialScoreBreakdown'],
+    };
+    supertest(server.listener)
+      .get('/api/users/info')
+      .set('accesstoken', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        expect(response.body.statusCode).toBe(200);
+        expect(response.body.data).toMatchSchema(schema);
         done();
       })
       .catch((e) => { throw e; });
