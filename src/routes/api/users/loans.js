c2c30d2cfdcaac88360e760c8f14df27862a9515
Saurav Sahu <sauravsahu.12@live.com>
2018-03-07 17:21:44 +0530

    Code Review: Issue 17: Loan Functionality: Create API for request for loan (#101)
    *  Update coverage script command.
    
    Update coverage script command.
    
    *  add joi validation.
    
    add joi validation.
    
    *  Add POST /api/users/loans to request for a loan.
    
    Add POST /api/users/loans to request for a loan.
    
    *  Round emi to nearest 10.
    
    Round emi to nearest 10.
    
    *  Update seeder for loans with valid attribute data.
    
    Update seeder for loans with valid attribute data.
    
    *  add test for invalid user access token
    
    add test for invalid user access token

--- src/routes/api/users/loans.js
+++ src/routes/api/users/loans.js
@@ -52,4 +52,93 @@ module.exports = [
       })
       .catch(response),
   },
+  {
+    path: '/api/users/loans',
+    method: 'POST',
+    config: {
+      description: 'Request for a loan.',
+      tags: ['api'],
+      validate: {
+        headers: {
+          accesstoken: joi.string().required(),
+        },
+        payload: {
+          totalAmount: joi.number()
+            .min(100000)
+            .max(1000000)
+            .multiple(25000)
+            .required(),
+          totalInstallments: joi.number()
+            .min(12)
+            .max(36)
+            .multiple(6)
+            .required(),
+        },
+        options: {
+          allowUnknown: true,
+        },
+      },
+    },
+    handler: (request, response) => {
+      const totalAmount = Number(request.payload.totalAmount);
+      const totalInstallments = Number(request.payload.totalInstallments);
+
+      return facebookHelpers
+        .inspectUserAccessToken(request.headers.accesstoken)
+        .then((inspectionResult) => {
+          if (inspectionResult.isValid) {
+            return models.facebooks.findOne({
+              where: {
+                id: inspectionResult.userId,
+              },
+            })
+              .then(facebookUser => Promise.all([facebookUser,
+                models.loans.count({
+                  where: {
+                    userId: facebookUser.userId,
+                    outstandingAmount: {
+                      $gt: 0,
+                    },
+                  },
+                })]))
+              .then(([facebookUser, loansCount]) => {
+                if (loansCount > 0) {
+                  return response({
+                    statusCode: 400,
+                    error: 'Bad request',
+                    message: 'You already have a pending loan.',
+                  });
+                }
+
+                const emi = Math.round(((1.1 * totalAmount) / totalInstallments) / 10) * 10;
+                const outstandingAmount = emi * totalInstallments;
+
+                return models.loans.create({
+                  userId: facebookUser.userId,
+                  totalAmount,
+                  totalInstallments,
+                  outstandingAmount,
+                  outstandingInstallments: totalInstallments,
+                })
+                  .then(loan => response({
+                    data: {
+                      totalAmount: loan.totalAmount,
+                      outstandingAmount: loan.outstandingAmount,
+                      outstandingInstallments: loan.outstandingInstallments,
+                      totalInstallments: loan.totalInstallments,
+                    },
+                    statusCode: 201,
+                  }))
+                  .catch(response);
+              });
+          }
+          return response({
+            error: 'Invalid token',
+            message: 'Invalid user access token',
+            statusCode: 400,
+          });
+        })
+        .catch(response);
+    },
+  },
 ];
--- test/routes/api/users/loans.test.js
+++ test/routes/api/users/loans.test.js
@@ -66,3 +66,223 @@ describe('route GET /api/users/loans', () => {
     });
   });
 });
+
+describe('route POST /api/users/loans', () => {
+  afterEach(() => models.loans.destroy({ where: { userId: 5 } }));
+
+  describe('should return 400 statusCode', () => {
+    test('when user access token is invalid', () =>
+      supertest(server.listener)
+        .post('/api/users/loans')
+        .send({})
+        .set('accesstoken', 'invalid-access-token')
+        .then((response) => {
+          expect(response.body.statusCode).toBe(400);
+        })
+        .catch((e) => { throw e; }));
+
+    describe('validation tests', () => {
+      test('when totalAmount and totalInstallments are not present in payload', () =>
+        supertest(server.listener)
+          .post('/api/users/loans')
+          .send({})
+          .set('accesstoken', process.env.ACCESS_TOKEN)
+          .then((response) => {
+            expect(response.body.statusCode).toBe(400);
+          })
+          .catch((e) => { throw e; }));
+
+      describe('totalAmount', () => {
+        test('when totalAmount is less than 100000', () =>
+          supertest(server.listener)
+            .post('/api/users/loans')
+            .send({
+              totalAmount: 75000,
+              totalInstallments: 12,
+            })
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(400);
+            })
+            .catch((e) => { throw e; }));
+
+        test('when totalAmount is more than 1000000', () =>
+          supertest(server.listener)
+            .post('/api/users/loans')
+            .send({
+              totalAmount: 1100000,
+              totalInstallments: 12,
+            })
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(400);
+            })
+            .catch((e) => { throw e; }));
+
+        test('when totalAmount is not a multiple of 25000', () =>
+          supertest(server.listener)
+            .post('/api/users/loans', {
+              totalAmount: 7500020,
+              totalInstallments: 12,
+            })
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(400);
+            })
+            .catch((e) => { throw e; }));
+      });
+
+      describe('totalInstallments', () => {
+        test('when totalInstallments is less than 12', () =>
+          supertest(server.listener)
+            .post('/api/users/loans')
+            .send({
+              totalAmount: 125000,
+              totalInstallments: 6,
+            })
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(400);
+            })
+            .catch((e) => { throw e; }));
+
+        test('when totalInstallments is more than 36', () =>
+          supertest(server.listener)
+            .post('/api/users/loans')
+            .send({
+              totalAmount: 100000,
+              totalInstallments: 48,
+            })
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(400);
+            })
+            .catch((e) => { throw e; }));
+
+        test('when totalInstallments is not a multiple of 6', () =>
+          supertest(server.listener)
+            .post('/api/users/loans')
+            .send({
+              totalAmount: 100000,
+              totalInstallments: 19,
+            })
+            .set('accesstoken', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              expect(response.body.statusCode).toBe(400);
+            })
+            .catch((e) => { throw e; }));
+      });
+    });
+
+    describe('when user already has a loan', () => {
+      beforeEach(() => {
+        const totalAmount = 250000;
+        const totalInstallments = 24;
+
+        return models.loans.create({
+          userId: 5,
+          totalAmount,
+          totalInstallments,
+          outstandingAmount: 1.1 * totalAmount,
+          outstandingInstallments: totalInstallments,
+        });
+      });
+
+      afterEach(() => models.loans.destroy({
+        where: {
+          userId: 5,
+          totalAmount: 250000,
+          totalInstallments: 24,
+        },
+      }));
+
+      test('with outstandingAmount greater than 0', () =>
+        supertest(server.listener)
+          .post('/api/users/loans')
+          .send({
+            totalAmount: 100000,
+            totalInstallments: 24,
+          })
+          .set('accesstoken', process.env.ACCESS_TOKEN)
+          .then((response) => {
+            expect(response.body.statusCode).toBe(400);
+          }));
+    });
+  });
+
+  describe('should return 201 statusCode', () => {
+    afterEach(() => models.loans.destroy({
+      where: { userId: 5 },
+    }));
+
+    test('when user applies for a loan', () =>
+      supertest(server.listener)
+        .post('/api/users/loans')
+        .send({
+          totalAmount: 100000,
+          totalInstallments: 24,
+        })
+        .set('accesstoken', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          expect(response.body.statusCode).toBe(201);
+        }));
+  });
+
+  describe('should return valid loan object', () => {
+    afterEach(() => models.loans.destroy({
+      where: { userId: 5 },
+    }));
+
+    test('when user applies for a loan', (done) => {
+      supertest(server.listener)
+        .post('/api/users/loans')
+        .send({
+          totalAmount: 100000,
+          totalInstallments: 12,
+        })
+        .set('accesstoken', process.env.ACCESS_TOKEN)
+        .then(response => Promise.all([response, models.loans.findOne({
+          where: {
+            userId: 5,
+          },
+        })]))
+        .then(([response, loan]) => {
+          expect({
+            totalAmount: loan.totalAmount,
+            outstandingAmount: loan.outstandingAmount,
+            outstandingInstallments: loan.outstandingInstallments,
+            totalInstallments: loan.totalInstallments,
+          }).toEqual({
+            ...response.body.data,
+          });
+          done();
+        });
+    });
+  });
+
+  describe('should return loan object with correct values', () => {
+    afterEach(() => models.loans.destroy({
+      where: { userId: 5 },
+    }));
+
+    test('when user applies for a loan', (done) => {
+      const totalAmount = 250000;
+      const totalInstallments = 12;
+
+      const emi = Math.round(((1.1 * totalAmount) / totalInstallments) / 10) * 10;
+
+      supertest(server.listener)
+        .post('/api/users/loans')
+        .send({
+          totalAmount,
+          totalInstallments,
+        })
+        .set('accesstoken', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          expect(response.body.data.outstandingAmount).toBe(Math.round(emi * totalInstallments));
+          expect(response.body.data.outstandingInstallments).toBe(totalInstallments);
+          done();
+        });
+    });
+  });
+});
