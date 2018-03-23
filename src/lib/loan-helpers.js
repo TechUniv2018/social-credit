680a869c2f7e59a04c9ba5eeafee223473fd6c5c
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-23 09:09:26 +0530

    Max social score is now 1000

--- src/lib/loan-helpers.js
+++ src/lib/loan-helpers.js
@@ -7,7 +7,7 @@ const maximumEligibleAmount = (socialScore) => {
   if (typeof socialScore !== 'number') {
     return 0;
   }
-  const score = Math.min(Math.max(socialScore, 0), 100);
+  const score = Math.min(Math.max(socialScore, 0), 1000);
   return Math.floor((score * 10000) / 25000) * 25000;
 };
 
